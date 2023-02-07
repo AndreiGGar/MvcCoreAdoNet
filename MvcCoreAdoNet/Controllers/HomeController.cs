589a6370aa03096196d17c876ffd3edb6d368114
andreiggar <andreiggar@gmail.com>
2023-02-07 13:43:47 +0100

    First Commit

---
+++ MvcCoreAdoNet/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contenido()
+        {
+            return View();
+        }
+
+        public IActionResult VistaMascota()
+        {
+            Mascota mascota = new Mascota();
+            mascota.Nombre = "Wolf";
+            mascota.Raza = "Hasky";
+            return View(mascota);
+        }
+    }
+}
---
+++ MvcCoreAdoNet/Models/Mascota.cs
@@ -0,0 +1,8 @@
+﻿namespace MvcCoreAdoNet.Models
+{
+    public class Mascota
+    {
+        public string Nombre { get; set; }
+        public string Raza { get; set; }
+    }
+}
